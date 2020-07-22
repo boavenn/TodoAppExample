7c06ff5dc55af1d37668e2691c480f0544d62e67
Witold Brzozowski <brzozowskyy@gmail.com>
2020-07-22 21:27:27 +0200

    init

---
+++ src/main/java/io/github/boavenn/todoappexample/TodoAppExampleApplication.java
@@ -0,0 +1,14 @@
+package io.github.boavenn.todoappexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppExampleApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/boavenn/todoappexample/TodoAppExampleApplicationTests.java
@@ -0,0 +1,14 @@
+package io.github.boavenn.todoappexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppExampleApplicationTests
+{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
